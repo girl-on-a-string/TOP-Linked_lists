7aaced136b50f6c7068ff2de809d72dfb3bd214d
girl-on-a-string <frenchfries55@protonmail.com>
2025-03-07 10:14:40 -0600

    created main factories and functions

---
+++ index.js
@@ -0,0 +1,56 @@
+// full list
+
+const LinkedList = () => {
+    const append = (value) => { // adds new value to end of list
+
+    }
+
+    const prepend = (value) => { // adds new value to beginning of list
+
+    }
+
+    const size = () => { // returns size of list
+
+    }
+
+    const head = () => { // returns first item in list
+
+    }
+
+    const tail = () => { // returns last item in list
+
+    }
+
+    const at = (index) => { // returns node at current index
+
+    }
+
+    const pop = () => { // removes last item in list
+
+    }
+
+    const contains = (value) => { // returns true if value is in list, otherwise returns false
+
+    }
+
+    const find = (value) => { // returns index of value if value is present, otherwise returns null
+
+    } 
+
+    const toString = () => { // represents linkedlists objects as strings, so you can print and preview them in the console
+
+    }
+
+    return {
+        
+    }
+}
+
+//
+
+const Node = () => {
+    let value = null;
+    let nextNode = null;
+
+
+}
